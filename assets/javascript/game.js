4f3a9fffa022e8aeffed3332061d13e54cd763f0
Luke Skywalker <joshua.mobley.jm@gmail.com>
2018-12-06 14:09:20 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,78 @@
 // Prevents clock from being sped up
 var TimerOn = false;
 var intervalId;
-var time = 60;
+var time = 120;
 
-function start() {
+// Starts the timeer
+function startTime() {
     if (!TimerOn) {
         intervalId = setInterval(count, 1000);
         TimerOn = true;
     }
+
 };
 
-$('#StartBttn').click(function () {
-    start();
-});
-
+// sets decriment of timer and displays to the screen
 function count() {
-
-    //  TODO: increment time by 1, remember we cant use "this" here.
     time -= 1;
-    //  TODO: Get the current time, pass that into the timeConverter function,
-    //        and save the result in a variable.
     var countTime = timeConverter(time);
-    //  TODO: Use the variable you just created to show the converted time in the "display" div.
+
     $('#DisplayTime').html('Time Remaining: ' + countTime);
-  
-  }
-  
-  //  THIS FUNCTION IS DONE FOR US!
-  //  We do not need to touch it.
-  
-  function timeConverter(t) {
-  
-    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+}
+//  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+function timeConverter(t) {
+
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-  
+
     if (seconds < 10) {
-      seconds = "0" + seconds;
+        seconds = "0" + seconds;
     }
-  
+
     if (minutes === 0) {
-      minutes = "00";
+        minutes = "00";
     }
-  
+
     else if (minutes < 10) {
-      minutes = "0" + minutes;
+        minutes = "0" + minutes;
     }
-  
+
     return minutes + ":" + seconds;
-  }
+}
+
+//Object containing an object of questions and object of answers
+var QA = {
+    questions: {
+        one: "What is Bender's catch phrase?",
+        two: "How is Fry related to the professor?",
+        three: "Where is Leela from?"
+    },
+    answers: {
+        one: "Bite My Shiney Metal Ass",
+        two: "Uncle",
+        three: "Orphange"
+    }
+}
+
+// Holds the number that is used for the questions, answers and displaying
+var num = 0;
+var numstring = ['one', 'two', 'three'];
+
+// Pulls the question based on number and possible answers & displays to appropiate div
+function DisQuestion() {
+    var idnum = '#' + 'Question' + numstring[num];
+    var questnum = 'QA.questions.' + numstring[num];
+    var ansnum = 'QA.answers.' + numstring[num];
+    $(idnum).append(questnum);
+    $(idnum).append(ansnum);
+
+    console.log(idnum);
+}
+
+// Starts the game
+$('#StartBttn').click(function () {
+    startTime();
+    $('#StartBttn').detach();
+});
+
+DisQuestion();
